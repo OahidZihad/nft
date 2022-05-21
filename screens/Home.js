89a811b8fc5fec6044f9dd5f16a3689455d2ab9a
OahidZihad <oahidzihad1@gmail.com>
2022-05-21 18:15:17 +0600

    search

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,22 @@ import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length) {
+      setNftData(filteredData);
+    } else {
+      setNftData(NFTData);
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary}></FocusedStatusBar>
@@ -11,11 +27,13 @@ const Home = () => {
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item}></NFTCard>}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader></HomeHeader>}
+            ListHeaderComponent={
+              <HomeHeader onSearch={handleSearch}></HomeHeader>
+            }
           />
 
           <View
